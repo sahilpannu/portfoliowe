251bdb7e5333e7cf46ca12d1f5de832b5f085a11
Sahil Pannu <81547349+sahilpannu@users.noreply.github.com>
2025-05-02 09:44:56 +0530

    cursor added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+var main = document.querySelector("#page1")
+var crsr = document.querySelector("#cursor")
+ main.addEventListener("mousemove",function(dets){
+      crsr.style.left = dets.x+"px"
+      crsr.style.top = dets.y+"px"
+})
