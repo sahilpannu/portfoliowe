1e8eb4bd418b42cdfb656adb8d16c9f853e73f00
Sahil Pannu <81547349+sahilpannu@users.noreply.github.com>
2025-05-02 22:09:02 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var main = document.querySelector("#page1")
 var crsr = document.querySelector("#cursor")
-
 main.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
     gsap.to(crsr, {
-        crsr.style.left = dets.x + "px"
-        crsr.style.top = dets.y + "px"
         duration: 0.15,
         ease: "power2.out"
     });
